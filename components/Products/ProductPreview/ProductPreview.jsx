9c9530912eac1d95b1f27673ad4f5674e4a887c2
Guru Saran M <gurusaranm0025@gmail.com>
2023-11-30 23:15:40 +0530

    removeed old code

--- components/Products/ProductPreview/ProductPreview.jsx
+++ components/Products/ProductPreview/ProductPreview.jsx
@@ -1,28 +1,12 @@
 import classes from "@/components/Products/ProductPreview/ProductPreview.module.css";
-import Image from "next/image";
 import Sample from "@/public/assets/Products/sample01.jpg";
-
-// function ProductPreview() {
-//   return (
-//     <div className={classes.previewWrapper}>
-//       <div className={classes.imgWrapper}>
-//         <Image className={classes.img} alt="plant image" src={Sample} />
-//         <button className={classes.btn}>Add to cart</button>
-//       </div>
-//       <div className={classes.detailsWrapper}>
-//         <span className={classes.name}>Product name</span>
-//         <span className={classes.price}>price</span>
-//       </div>
-//     </div>
-//   );
-// }
 
 function ProductPreview() {
   return (
     <>
       <div className={classes.previewWrapper}>
         <div className={classes.imgWrapper}>
-          <img className={classes.img} src={Sample.src} />
+          <img className={classes.img} src={Sample.src} loading="lazy" />
           <button className={classes.btn}>Add to cart</button>
         </div>
       </div>
